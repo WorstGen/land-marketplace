decd8e52b739b596bed117d92b0f326e521137f4
WorstGen <47105856+WorstGen@users.noreply.github.com>
2025-09-05 12:15:18 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LandPlotMarketplace from './components/LandPlotMarketplace';
+
+function App() {
+  return (
+    <div className="App">
+      <LandPlotMarketplace />
+    </div>
+  );
+}
+
+export default App;
