dfd6ead942d90ab5a66b0545092b5f798a28fb85
WorstGen <47105856+WorstGen@users.noreply.github.com>
2025-09-05 22:37:55 -0700

    Update and rename App.js to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Land Marketplace</h1>
+        <p>Welcome to the Solana Land Marketplace</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
